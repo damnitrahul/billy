46715c13092e36e68b08c34965015d7694cc225f
Rahul Raj <damnitrahul@gmail.com>
2020-04-27 10:20:38 +0530

    code splitting

---
+++ src/AllRoutes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Redirect, Switch, Route } from 'react-router-dom';
+// Pages Components
+import Dashboard from './components/pages/dashboard/Dashboard';
+import NewInvoice from './components/pages/create/NewInvoice';
+import Invoices from './components/pages/invoices/Invoices';
+import Settings from './components/pages/settings/Settings';
+import InvoiceDetails from './components/pages/view/InvoiceDetails';
+import EditSettings from './components/pages/settings/EditSettings';
+import NotFound from './components/loaders/404/NotFound';
+
+function AllRoutes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Dashboard} />
+      <Route exact path="/create" component={NewInvoice} />
+      <Route exact path="/invoices" component={Invoices} />
+      <Route exact path="/settings" component={Settings} />
+      <Route exact path="/settings/edit" component={EditSettings} />
+      <Route exact path="/invoice/:id" component={InvoiceDetails} />
+      <Route exact path="/register" render={() => <Redirect to="/" />} />
+      <Route exact path="/login" render={() => <Redirect to="/" />} />
+      <Route component={NotFound} />
+      />
+    </Switch>
+  );
+}
+
+export default AllRoutes;
--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-
+import Loadable from 'react-loadable';
 // Vendor
 import { Redirect, Switch, Route } from 'react-router-dom';
 // Styled Components
 import { BodyContainer } from './components/styledComponents/layout/BodyContainer';
 import SideBar from './components/sidebar/SideBar';
-// Pages Components
-import Dashboard from './components/pages/dashboard/Dashboard';
-import NewInvoice from './components/pages/create/NewInvoice';
-import Invoices from './components/pages/invoices/Invoices';
-import Settings from './components/pages/settings/Settings';
-import InvoiceDetails from './components/pages/view/InvoiceDetails';
 
 // Firestore
 import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 import { useSelector } from 'react-redux';
 import SignUp from './components/pages/auth/SignUp';
 import Login from './components/pages/auth/Login';
-import EditSettings from './components/pages/settings/EditSettings';
 import BottomNavigation from './components/bottomNavigation/BottomNavigation';
 import AlertDialog from './components/dialog/Dialog';
 import ForgotPassword from './components/pages/auth/ForgotPassword';
 import AlertSnackbar from './components/snackbar/SnackBar';
 import AppLoader from './components/loaders/app/AppLoader';
-import NotFound from './components/loaders/404/NotFound';
+import AllRoutes from './AllRoutes';
+
+const LoadableComponent = Loadable({
+  loader: () => import('./AllRoutes'),
+  loading: AppLoader
+});
 
 function App() {
   const auth = useSelector((state) => state.firebase.auth);
@@ -75,18 +73,7 @@ function App() {
     <BodyContainer>
       <SideBar />
       <div>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route exact path="/create" component={NewInvoice} />
-          <Route exact path="/invoices" component={Invoices} />
-          <Route exact path="/settings" component={Settings} />
-          <Route exact path="/settings/edit" component={EditSettings} />
-          <Route exact path="/invoice/:id" component={InvoiceDetails} />
-          <Route exact path="/register" render={() => <Redirect to="/" />} />
-          <Route exact path="/login" render={() => <Redirect to="/" />} />
-          <Route component={NotFound} />
-          />
-        </Switch>
+        <LoadableComponent />
       </div>
       <BottomNavigation />
       <AlertDialog />
